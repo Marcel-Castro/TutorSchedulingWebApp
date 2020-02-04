a56ab1b224cd2553f50651d48d4abd6d1b2b2e67
Marcel <castrolecram@gmail.com>
2020-02-03 19:59:49 -0500

    Added functions to populate the course and tutor selector elements based on tutor/course data

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 function populateTutorSelector (tutors) {
-    var courseSelector = document.getElementById("tutor");
+    var tutorSelector = document.getElementById("tutor");
 
     for (var i = 0; i < tutors.length; i++) {
         var newTutor = document.createElement("option");
+        var newName = document.createTextNode(tutors[i].name);
+        newTutor.appendChild(newName);
+        newTutor.setAttribute("value", tutors[i].name);
+        tutorSelector.appendChild(newTutor);
     }
 }
 
 function populateCourseSelector (courses) {
     var courseSelector = document.getElementById("course");
+
+    for (var i = 0; i < courses.length; i++) {
+        var newCourse = document.createElement("option");
+        var newName = document.createTextNode(courses[i]);
+        newCourse.appendChild(newName);
+        newCourse.setAttribute("value", courses[i]);
+        courseSelector.appendChild(newCourse);
+    }
 }
 
 function main () {
-    fillTable();
-    // populateTableShifts(condenseTutorShifts(tutors, "COP2"));
+    populateTutorSelector(tutors);
+    populateCourseSelector(courses);
+    // fillTable();
+    var filter = document.getElementById("filter");
+
+
+    populateTableShifts(condenseTutorShifts(tutors, "COP2"));
 }
