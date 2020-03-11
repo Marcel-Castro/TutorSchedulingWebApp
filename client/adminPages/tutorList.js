d1ca623300c366934712163a7eee39b74018c638
Marcel <castrolecram@gmail.com>
2020-03-11 05:25:47 -0400

    Clicking on tutor card now displays tutor details on tutorList page but it only works for the final tutor's information

--- client/adminPages/tutorList.js
+++ client/adminPages/tutorList.js
@@ -1,3 +1,49 @@
+// Returns an array of shifts sorted by day first and starting time second 
+// (different from the one found in table which goes by starting time first) TODO -------------
+function sortShiftsDayFirst(tutorShifts) {
+    var shifts = [];
+    var i;
+
+    shifts.push(tutorShifts[0]);
+
+    for (i = 1; i < tutorShifts.length; i++) {
+        var j = 0;
+        var placed = false;
+
+        while (j < i && placed === false) {
+            if (tutorShifts[i].startTime === shifts[j].startTime) {
+                var k = j;
+
+                while (shifts[k].startTime === shifts[j].startTime) {
+                    if (getDayNumber(tutorShifts[i].day) < getDayNumber(shifts[k].day)) {
+                        shifts.splice(k, 0, tutorShifts[i]);
+                        placed = true;
+                        break;
+                    } else if (shifts[k + 1] === undefined || shifts[k + 1].startTime !== shifts[j].startTime) {
+                        shifts.splice(k + 1, 0, tutorShifts[i]);
+                        placed = true;
+                        break;
+                    } else {
+                        k++;
+                    }
+                }
+            } else if (tutorShifts[i].startTime < shifts[j].startTime) {
+                shifts.splice(j, 0, tutorShifts[i]);
+                placed = true;
+            }
+            else if (j >= i - 1) {
+                shifts.push(tutorShifts[i]);
+                placed = true;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    return shifts;
+}
+
+
 function populateTutorList (tutors) {
     var tutorList = document.getElementById("tutorList");
 
@@ -10,6 +56,44 @@ function populateTutorList (tutors) {
         var newText = document.createTextNode(tutors[i].name);
         newSpan.appendChild(newText);
         newContainer.appendChild(newSpan);
+
+        // These need to be declared and initialized outside of the event listener
+        // tutors is not available to the event listeners after this function runs
+        var thisCourses = tutors[i].courses;
+        var thisShifts = tutors[i].shifts;
+
+        console.log(thisCourses);
+
+        // Add card onClick event listener
+        newContainer.addEventListener("click", (event) => {
+            var coursesList = document.getElementById("coursesCovered");
+            var shiftsList = document.getElementById("shiftsCovered");
+            var courses = thisCourses;
+            var shifts = thisShifts;
+
+            // Clear current contents of the element
+            coursesList.innerHTML = "";
+            // Add contents related to this card
+            for (var j = 0; j < courses.length; j++) {
+                var coursesSpan = document.createElement("span");
+                var courseText = document.createTextNode(courses[j]);
+                coursesSpan.appendChild(courseText);
+                coursesSpan.classList.add("listSpan");
+                coursesList.appendChild(coursesSpan);
+            }
+
+            // Clear current contents of the element
+            shiftsList.innerHTML = "";
+            // Add contents related to this card
+            for (var j = 0; j < shifts.length; j++) {
+                var tutorsSpan = document.createElement("span");
+                var tutorText = document.createTextNode(shifts[j].day 
+                    + " from " + getTimeFromID(shifts[j].startTime) + " to " + getTimeFromID(shifts[j].endTime));
+                tutorsSpan.appendChild(tutorText);
+                tutorsSpan.classList.add("listSpan");
+                shiftsList.appendChild(tutorsSpan);
+            }
+        })
 
         // Create and add edit button to card
         var editButton = document.createElement("button");
@@ -34,6 +118,7 @@ function populateTutorList (tutors) {
         tutorList.appendChild(newContainer);
     }
 }
+
 
 function main () {
     populateTutorsArray(populateTutorList);
