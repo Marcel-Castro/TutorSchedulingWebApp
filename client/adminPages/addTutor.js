c785457e7b00635f29388f60c497f27d46f2d2ad
Marcel <castrolecram@gmail.com>
2020-03-31 23:35:52 -0400

    Tutor shift and course arrays are now sorted when a tutor is created or updated

--- client/adminPages/addTutor.js
+++ client/adminPages/addTutor.js
@@ -1,48 +1,25 @@
-// Returns an array of shifts sorted by day first and starting time second 
-// (different from the one found in table which goes by starting time first) TODO -------------
-// function sortShiftsDayFirst(tutorShifts) {
-//     var shifts = [];
-//     var i;
-
-//     shifts.push(tutorShifts[0]);
-
-//     for (i = 1; i < tutorShifts.length; i++) {
-//         var j = 0;
-//         var placed = false;
-
-//         while (j < i && placed === false) {
-//             if (tutorShifts[i].startTime === shifts[j].startTime) {
-//                 var k = j;
-
-//                 while (shifts[k].startTime === shifts[j].startTime) {
-//                     if (getDayNumber(tutorShifts[i].day) < getDayNumber(shifts[k].day)) {
-//                         shifts.splice(k, 0, tutorShifts[i]);
-//                         placed = true;
-//                         break;
-//                     } else if (shifts[k + 1] === undefined || shifts[k + 1].startTime !== shifts[j].startTime) {
-//                         shifts.splice(k + 1, 0, tutorShifts[i]);
-//                         placed = true;
-//                         break;
-//                     } else {
-//                         k++;
-//                     }
-//                 }
-//             } else if (tutorShifts[i].startTime < shifts[j].startTime) {
-//                 shifts.splice(j, 0, tutorShifts[i]);
-//                 placed = true;
-//             }
-//             else if (j >= i - 1) {
-//                 shifts.push(tutorShifts[i]);
-//                 placed = true;
-//             } else {
-//                 j++;
-//             }
-//         }
-//     }
-
-//     return shifts;
-// }
-
+// Shift compare function to use with javascript sort()
+/*
+    returns positive int for shiftA > shiftB
+    returns zero for shiftA == shiftB
+    returns negative shiftA < shiftB
+*/
+function compareShifts(shiftA, shiftB) {
+    if (getDayNumber(shiftA.day) < getDayNumber(shiftB.day)) {
+        return -1;
+    } else if (getDayNumber(shiftA.day) > getDayNumber(shiftB.day)) {
+        return 1;
+    } else {
+        // If days are equal, compare start times of each shift
+        if (shiftA.startTime < shiftB.startTime) {
+            return -1;
+        } else if (shiftA.startTime > shiftB.startTime) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
 
 // Creates button elements with clone and delete icons
 function createCloneAndDeleteButton() {
@@ -367,12 +344,18 @@ function createNewTutor() {
         newTutor.shifts.push(newShift);
     }
 
-    // Create courses from ccourseList
+    // Sort shift list of this tutor
+    newTutor.shifts.sort(compareShifts);
+
+    // Create courses from courseList
     for (var i = 0; i < courseList.length; i++) {
         var newCourse = courseList[i].childNodes[1].value;
 
         newTutor.courses.push(newCourse);
     }
+
+    // Sort course list of this tutor
+    newTutor.courses.sort();
 
     return newTutor;
 }
